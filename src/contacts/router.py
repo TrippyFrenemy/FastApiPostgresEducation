92ef420f9a76f06dd61711495d220ec194d73536
Dmytro Avruschenko <trippyfren@gmail.com>
2023-12-10 20:16:57 +0200

    Add searching by name, surname, emai and notify by upcoming birthdays

--- src/contacts/router.py
+++ src/contacts/router.py
@@ -1,7 +1,11 @@
-from fastapi import APIRouter, Depends, HTTPException
+import logging
+from datetime import datetime, timedelta
+from operator import and_
+
+from fastapi import APIRouter, Depends, HTTPException, Query
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from sqlalchemy import select, insert
+from sqlalchemy import select, insert, or_, extract
 from src.contacts.models import Contact
 from src.contacts.schemas import ContactCreate, ContactInDB, ContactUpdate
 from src.database import get_async_session
@@ -63,3 +67,46 @@ async def delete_contact(contact_id: int, db: AsyncSession = Depends(get_async_s
     await db.delete(contact_to_delete)
     await db.commit()
     return {"message": "Contact deleted successfully"}
+
+
+@router.get("/get_by_attr")
+async def search_contacts(
+        name: str = Query(None),
+        surname: str = Query(None),
+        email: str = Query(None),
+        db: AsyncSession = Depends(get_async_session)
+):
+    logging.info(f"Received query parameters: name={name}, surname={surname}, email={email}")
+    query_conditions = []
+
+    if name:
+        query_conditions.append(Contact.first_name.like(f"%{name}%"))
+    if surname:
+        query_conditions.append(Contact.last_name.like(f"%{surname}%"))
+    if email:
+        query_conditions.append(Contact.email.like(f"%{email}%"))
+
+    query = select(Contact)
+    if query_conditions:
+        query = query.filter(or_(*query_conditions))
+
+    result = await db.execute(query)
+    contacts = result.scalars().all()
+    return contacts
+
+
+@router.get("/upcoming-birthdays")
+async def upcoming_birthdays(db: AsyncSession = Depends(get_async_session)):
+    current_date = datetime.now().date()
+    upper_limit = current_date + timedelta(days=7)
+
+    result = await db.execute(
+        select(Contact).where(
+            and_(
+                extract('month', Contact.birthday) == current_date.month,
+                extract('day', Contact.birthday).between(current_date.day, upper_limit.day)
+            )
+        )
+    )
+    contacts = result.scalars().all()
+    return contacts
