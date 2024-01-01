ba74bc2dc28911760c89ba8ec6c1bac2e7e14baa
Dmytro Avruschenko <trippyfren@gmail.com>
2024-01-01 16:26:17 +0200

    Add verify and forgot password

--- src/auth/manager.py
+++ src/auth/manager.py
@@ -16,6 +16,16 @@ class UserManager(IntegerIDMixin, BaseUserManager[User, int]):
     async def on_after_register(self, user: User, request: Optional[Request] = None):
         print(f"User {user.id} has registered.")
 
+    async def on_after_request_verify(
+        self, user: User, token: str, request: Optional[Request] = None
+    ):
+        print(f"Verification requested for user {user.id}. Verification token: {token}")
+
+    async def on_after_forgot_password(
+        self, user: User, token: str, request: Optional[Request] = None
+    ):
+        print(f"User {user.id} has forgot their password. Reset token: {token}")
+
     async def create(
         self,
         user_create: schemas.UC,
